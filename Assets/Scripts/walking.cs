32203b51f679f57aa68bd67e3fb6ab902fe09024
CJohnson778 <moflynn@terpmail.umd.edu>
2026-02-19 13:44:40 -0500

    flip walkers
    when reaching an edge the walker flips to walk the other way

--- Assets/Scripts/walking.cs
+++ Assets/Scripts/walking.cs
@@ -3,7 +3,8 @@ using UnityEngine.Rendering;
 
 public class walking : MonoBehaviour
 {
-    public float speed = 10f;
+    public float speed = 30f;
+    public bool left = true;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,14 +17,37 @@ public class walking : MonoBehaviour
     {
         Vector3 currentRotation = transform.eulerAngles;
         float currentX = currentRotation.x;
+        Vector3 currentSpot = transform.position;
+        float spot = transform.position.x;
 
-        transform.Rotate(new Vector3(0,1,0)*speed*Time.deltaTime );
-        if (currentX<70)
+        if (spot > 1 && left==true)
         {
-            transform.eulerAngles = new Vector3(90f, 0f, 0f);
-            transform.Translate(new Vector3(10, 0, 0)*speed*Time.deltaTime);
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            left = false;
+        }else if (spot < -1 && left ==false)
+        {
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            left = true;
         }
-        
-        
+        if(left ==true){
+            speed = 30f;
+            transform.Rotate(new Vector3(0,1,0)*speed*Time.deltaTime );
+            if (currentX<70)
+            {
+                transform.eulerAngles = new Vector3(90f, 0f, 0f);
+                transform.Translate(new Vector3(15, 0, 0)*speed*Time.deltaTime);
+            }
+        }
+        else
+        {
+            speed = -30f;
+            transform.Rotate(new Vector3(0,1,0)*speed*Time.deltaTime );
+            if (currentX<70)
+            {
+                transform.eulerAngles = new Vector3(90f, 0f, 0f);
+                transform.Translate(new Vector3(10, 0, 0)*speed*Time.deltaTime);
+            }
+        }
+
     }
 }
