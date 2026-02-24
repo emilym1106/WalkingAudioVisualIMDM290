e553bd39c83e2f61170d77c40a771a5326d3a17b
CJohnson778 <moflynn@terpmail.umd.edu>
2026-02-24 02:50:54 -0500

    prefab of synced walkers

--- Assets/Scripts/walking.cs
+++ Assets/Scripts/walking.cs
@@ -5,6 +5,8 @@ public class walking : MonoBehaviour
 {
     public float speed = 30f;
     public bool left = true;
+    public float turn1 = 3.15f;
+    public float turn2 = -2.7f;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,17 +22,19 @@ public class walking : MonoBehaviour
         Vector3 currentSpot = transform.position;
         float spot = transform.position.x;
 
-        if (spot > 1 && left==true)
+        if (spot > turn1 && left==true)
         {
             transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
             left = false;
-        }else if (spot < -1 && left ==false)
+            speed = -speed;
+        }else if (spot < turn2 && left ==false)
         {
             transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
             left = true;
+            speed = -speed;
         }
         if(left ==true){
-            speed = 30f;
+            
             transform.Rotate(new Vector3(0,1,0)*speed*Time.deltaTime );
             if (currentX<70)
             {
@@ -40,7 +44,7 @@ public class walking : MonoBehaviour
         }
         else
         {
-            speed = -30f;
+            
             transform.Rotate(new Vector3(0,1,0)*speed*Time.deltaTime );
             if (currentX<70)
             {
