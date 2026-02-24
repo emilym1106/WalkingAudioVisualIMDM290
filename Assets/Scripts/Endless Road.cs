0f0be3025af4deae4df8035cd53f9d51d852c77d
starlight862 <giraffekickslion@gmail.com>
2026-02-24 00:02:11 -0500

    pausing buildings, car, road at certain time stamps + more road prefabs

--- Assets/Scripts/Endless Road.cs
+++ Assets/Scripts/Endless Road.cs
@@ -3,20 +3,42 @@ using System.Collections;
 
 public class EndlessRoad : MonoBehaviour
 {
+    //CAR
+     public Transform carTransform;
+    public float moveSpeed = 3f;
+
+
+    //ROAD SECTIONS
     [SerializeField]
     GameObject[] sectionsPrefabs;
     GameObject[] allSections = new GameObject[20];
     GameObject[] sections = new GameObject[10];
     WaitForSeconds waitFor100ms = new WaitForSeconds(0.1f);
 
-    Transform carTransform;
-    const float sectionLength = 10;
+    //AUDIO REACTIVE BUILDINGS
+    public AudioSource audioSource;
+    public GameObject[] buildingPrefabs; 
+    int nBuildings = 32;
+    GameObject[] buildings;
+    public FrequencyFocusWindow freqFocusWindow;
+    public float amplification = 1.0f;
+    public float baseHeight = 0.0f;
+    public FFTWindow fftWindow;
+    public bool useDecibels;
+    public float[] spectrumData;
+
+    public float spacing = 10000f;
+    public float sectionLengthBuildings = 5000f;
+    const float sectionLength = 20;
+    public float pauseAtTime = 39f;
+    private bool isPaused = false;
+
     void Start()
     {
         carTransform = GameObject.FindGameObjectWithTag("car").transform;
         int prefabIndex = 0;
 
-        //selecting from a group of sections
+        //selecting from a group of road sections
         for (int i = 0; i < allSections.Length; i++)
         {
             allSections[i] = Instantiate(sectionsPrefabs[prefabIndex]);
@@ -39,17 +61,107 @@ public class EndlessRoad : MonoBehaviour
         }
 
         StartCoroutine(UpdateLessOftenCO());
+
+
+        ///////////////////////////////////////////////////////////////////////////////////
+        
+        //create random buildings
+        buildings = new GameObject[nBuildings];
+        sectionLengthBuildings = buildingPrefabs[0].GetComponent<Renderer>().bounds.size.z;
+
+        float spacing = sectionLengthBuildings * 3;
+
+        buildings = new GameObject[nBuildings];
+
+        for (int i = 0; i < nBuildings; i++)
+        {
+            GameObject prefab = buildingPrefabs[Random.Range(0, buildingPrefabs.Length)];
+
+            Vector3 pos = new Vector3(25f, 0f, i * spacing);
+            buildings[i] = Instantiate(prefab, pos, Quaternion.identity);
+            buildings[i].transform.Rotate(0f, 270f, 0f, Space.World); //rotate to face the road
+        }
     }
 
     IEnumerator UpdateLessOftenCO()
     {
-        while (true)
+        while (!isPaused)
         {
             UpdateSectionsPos();
             yield return waitFor100ms;
         }
     }
 
+    void Awake()
+    {
+        spectrumData = new float[4096];
+    }
+
+    //updating buildings
+    void Update()
+    {
+        Debug.Log(audioSource.time);
+        if (audioSource.time >= pauseAtTime)
+        {
+            isPaused = true;
+        }
+        if (!isPaused) {
+            carTransform.Translate(Vector3.forward * moveSpeed * Time.deltaTime); //move car forward
+
+            audioSource.GetSpectrumData(spectrumData, 0, fftWindow);
+            int blockSize = Mathf.Max(1, spectrumData.Length / buildings.Length / (int)freqFocusWindow);        
+            for (int i = 0; i < buildings.Length; ++i)
+            {
+                float sum = 0;
+                for (int j = 0; j < blockSize; j++)
+                {
+                    sum += spectrumData[i * blockSize + j];
+                }
+                sum /= blockSize;
+                float amplitude = Mathf.Clamp(sum, 1e-7f, 1f);
+                var scale = buildings[i].transform.localScale;
+                if (useDecibels)
+                {
+                    scale.y = -Mathf.Log10(amplitude) * amplification / 200;
+                } 
+                else
+                {
+                    scale.y = sum * amplification + baseHeight;
+                } 
+                buildings[i].transform.localScale = scale;
+            }
+
+            RecycleBuildings();
+        }
+    }
+
+    void RecycleBuildings()
+    {
+        for (int i = 0; i < buildings.Length; i++)
+        {
+            if (buildings[i].transform.position.z < carTransform.position.z - sectionLength)
+            {
+                float maxZ = GetFurthestZ();
+
+                buildings[i].transform.position = new Vector3(buildings[i].transform.position.x, 0, maxZ + spacing);
+            }
+        }
+    }
+
+    float GetFurthestZ()
+    {
+        float maxZ = buildings[0].transform.position.z;
+
+        for (int i = 1; i < buildings.Length; i++)
+        {
+            if (buildings[i].transform.position.z > maxZ)
+                maxZ = buildings[i].transform.position.z;
+        }
+
+        return maxZ;
+    }
+    
+    //updating road section positions
     void UpdateSectionsPos()
     {
         for (int i = 0 ; i < sections.Length; i++)
@@ -86,5 +198,14 @@ public class EndlessRoad : MonoBehaviour
         }
 
         return allSections[randIndex];
+    }
+
+    public enum FrequencyFocusWindow
+    {
+        Entire = 1,
+        FirstHalf = 2,
+        FirstQuarter = 4,
+        FirstEight = 8,
+        FirstSixteenth = 16
     }
 }
