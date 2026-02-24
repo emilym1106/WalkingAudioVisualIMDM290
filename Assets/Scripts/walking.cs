bfc343a73be4f5ee717a2b36baa4be1912393cb0
CJohnson778 <moflynn@terpmail.umd.edu>
2026-02-24 03:00:21 -0500

    code cleanup

--- Assets/Scripts/walking.cs
+++ Assets/Scripts/walking.cs
@@ -3,25 +3,26 @@ using UnityEngine.Rendering;
 
 public class walking : MonoBehaviour
 {
+    //how fast to rotate/how much to move forward
     public float speed = 30f;
+    //tells what direction they're walking
     public bool left = true;
+
+    //set bounds of the road to know where to turn around
     public float turn1 = 3.15f;
     public float turn2 = -2.7f;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
+        //get the rotation to be able to hinge on it
         Vector3 currentRotation = transform.eulerAngles;
         float currentX = currentRotation.x;
+        //get the position to be able to move it
         Vector3 currentSpot = transform.position;
         float spot = transform.position.x;
 
+        //when the walker gets to the sides of the road turn them around and have them walk the other way
         if (spot > turn1 && left==true)
         {
             transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
@@ -33,6 +34,7 @@ public class walking : MonoBehaviour
             left = true;
             speed = -speed;
         }
+        //move the walkers based on which way they're going
         if(left ==true){
             
             transform.Rotate(new Vector3(0,1,0)*speed*Time.deltaTime );
